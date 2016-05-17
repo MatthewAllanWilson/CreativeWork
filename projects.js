c2e818d47fe6bad73c599d81065aa6c06b52988d
MatthewAllanWilson <mattwils@gmail.com>
2016-05-17 12:48:59 -0700

    added three projects, worked on 960px layout

--- projects.js
+++ projects.js
@@ -20,5 +20,26 @@ var projectData =
       link:'http://www.amazon.com/Scrooge-Santa-GN-Matthew-Wilson/dp/1897548982/',
       image: 'images/Cover-New-Levels.jpg',
       id: 'scrooge'
+    },
+    {
+      title: 'Speak to Me in Poetry',
+      description: 'This was a \'test movie\' for the Amazon Studios system.  I tried, as much as possible, to make it a real feature film.',
+      link: 'https://youtu.be/L73JFxyZlco',
+      image: 'images/Speak_Cropped.jpg',
+      id: 'speak'
+    },
+    {
+      title: 'The Umpire',
+      description: 'This was my first \'test movie\' for the Amazon Studios system.  It won the $100,000 prize for the month of May, 2011.',
+      link: 'https://youtu.be/6n6lcelXHNA',
+      image: 'images/Umpire_Image.jpg',
+      id: 'umpire'
+    },
+    {
+      title: 'Movie Poster Taglines',
+      description: 'Occasionally I\'ve been paid to come up with taglines for movie posters.',
+      link: 'http://www.imdb.com/media/rm1867942656/tt1127896?ref_=tt_ov_i',
+      image: 'images/takingwoodstock.jpg',
+      id: 'poster'
     }
   ];
