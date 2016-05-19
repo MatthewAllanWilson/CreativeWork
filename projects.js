27cd967996e234aa0e73cebd166cb63c40d5146e
MatthewAllanWilson <mattwils@gmail.com>
2016-05-19 11:01:51 -0700

    added change on hover to indicate its a link

--- projects.js
+++ projects.js
@@ -2,35 +2,35 @@ var projectData =
   [
     {
       title: 'The Virgins',
-      description: 'This is a feature film I wrote, directed, produced, and edited.',
+      body: 'This is a feature film I wrote, directed, produced, and edited.',
       link: 'http://www.amazon.com/Virgins-Blake-Webb/dp/B00N2E8YWC/',
       image: 'images/TheVirgins_Poster.jpg',
       id: 'virgins'
     },
     {
       title: 'A Word to Rhyme with Orange',
-      description: 'I teamed up with illustrator Jessica Chrysler to turn a poem I wrote into a children\'s book.',
+      body: 'I teamed up with illustrator Jessica Chrysler to turn a poem I wrote into a children\'s book.',
       link:'http://www.amazon.com/Word-Rhyme-Orange-Matthew-Wilson/dp/1493767267/',
       image: 'images/Cover_Cropped_Promo.jpg',
       id: 'orange'
     },
     {
       title: 'Scrooge and Santa',
-      description: 'I teamed up with illustrator Josh Kenfield to turn a Christmas story I wrote into a graphic novel.',
+      body: 'I teamed up with illustrator Josh Kenfield to turn a Christmas story I wrote into a graphic novel.',
       link:'http://www.amazon.com/Scrooge-Santa-GN-Matthew-Wilson/dp/1897548982/',
       image: 'images/Cover-New-Levels.jpg',
       id: 'scrooge'
     },
     {
       title: 'Speak to Me in Poetry',
-      description: 'This was a \'test movie\' for the Amazon Studios system.  I tried, as much as possible, to make it a real feature film.',
+      body: 'This was a \'test movie\' for the Amazon Studios system.  I tried, as much as possible, to make it a real feature film.',
       link: 'https://youtu.be/L73JFxyZlco',
       image: 'images/Speak_Cropped.jpg',
       id: 'speak'
     },
     {
       title: 'The Umpire',
-      description: 'This was my first \'test movie\' for the Amazon Studios system.  It won the $100,000 prize for the month of May, 2011.',
+      body: 'This was my first \'test movie\' for the Amazon Studios system.  It won the $100,000 prize for the month of May, 2011.',
       //link: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6n6lcelXHNA" frameborder="0" allowfullscreen></iframe>',
       link: 'https://youtu.be/6n6lcelXHNA',
       image: 'images/Umpire_Image.jpg',
@@ -38,28 +38,28 @@ var projectData =
     },
     {
       title: 'Movie Poster Taglines',
-      description: 'Occasionally I\'ve been paid to come up with taglines for movie posters.',
+      body: 'Occasionally I\'ve been paid to come up with taglines for movie posters.',
       link: 'http://www.imdb.com/media/rm1867942656/tt1127896?ref_=tt_ov_i',
       image: 'images/takingwoodstock.jpg',
       id: 'poster'
     },
     {
       title: 'Krypto the Superdog',
-      description: 'My first professional writing credit was an episode of Krypto the Superdog called My Pet Boy.  It aired on Cartoon Network in 2005.',
+      body: 'My first professional writing credit was an episode of Krypto the Superdog called My Pet Boy.  It aired on Cartoon Network in 2005.',
       link: 'http://www.imdb.com/title/tt1350168/?ref_=ttep_ep6',
       image: 'images/Krypto.jpg',
       id: 'krypto'
     },
     {
       title: 'Brandy and Mr. Whiskers',
-      description: 'This was a show that aired on Disney Channel from 2004 - 2006.  I wrote the episode called Where Everybody Knows Your Shame',
+      body: 'This was a show that aired on Disney Channel from 2004 - 2006.  I wrote the episode called Where Everybody Knows Your Shame',
       link: 'http://www.imdb.com/title/tt0795626/?ref_=ttep_ep8',
       image: 'images/Brandy-and-Mr-Whiskers.jpg',
       id: 'brandy'
     },
     {
       title: 'Firehouse Tales',
-      description: 'I wrote two episodes for this show that ran on Cartoon Network in 2006.',
+      body: 'I wrote two episodes for this show that ran on Cartoon Network in 2006.',
       link: 'http://www.imdb.com/title/tt0454343/',
       image: 'images/Firehouse.jpg',
       id: 'firehouse'
